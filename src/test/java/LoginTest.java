ffdf69282a313d5b8c4adfc3a39c65f93c654ec9
SergGrishkov <Serg.Grishkov@gmail.com>
2018-10-16 19:20:57 +0300

    HomeWork #4

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static java.lang.Thread.sleep;
 
 public class LoginTest {
 
@@ -15,27 +14,26 @@ public class LoginTest {
         WebDriver webDriver = new FirefoxDriver();
         webDriver.get("https://linkedin.com");
 
-
         String linkSite = webDriver.getCurrentUrl();
+
         String login = "SergAutoTest@bigmir.net";
         String loginXpath = "//*[@id=\"login-email\"]";
         String pwd = "!qaz@wsx";
-        String pxdXpath = "//*[@id=\"login-password\"]";
+        String pwdXpath = "//*[@id=\"login-password\"]";
         String buttonXpath = "//*[@id=\"login-submit\"]";
-        String findLink = "https://www.linkedin.com/feed/";
+        String feedLink = "https://www.linkedin.com/feed/";
 
-        Assert.assertEquals(webDriver.getCurrentUrl(),linkSite);
+        Assert.assertEquals(webDriver.getCurrentUrl(),linkSite, "HomePage URL is correct!");
 
         WebElement searchFieldLogin = webDriver.findElement(By.xpath(loginXpath));
         searchFieldLogin.sendKeys(login);
-        WebElement searchFieldPwd = webDriver.findElement(By.xpath(pxdXpath));
+        WebElement searchFieldPwd = webDriver.findElement(By.xpath(pwdXpath));
         searchFieldPwd.sendKeys(pwd);
         WebElement searchFieldButton = webDriver.findElement(By.xpath(buttonXpath));
         searchFieldButton.click();
 
-        sleep(3000);
 
-        Assert.assertEquals(webDriver.getCurrentUrl(),findLink, "You open general page from your account.");
+        Assert.assertEquals(webDriver.getCurrentUrl(),feedLink, "AccountPage URL is not correct!");
 
         webDriver.quit();
 
