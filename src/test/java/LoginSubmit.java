21f3fb80d120640539cbe1058c5761b140e3c718
SergGrishkov <Serg.Grishkov@gmail.com>
2018-10-29 14:10:44 +0200

    HomeWork #8

--- src/test/java/LoginSubmit.java
+++ src/test/java/LoginSubmit.java
@@ -3,27 +3,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class LoginSubmit {
 
     private WebDriver webDriver;
 
-//    @FindBy(xpath = "//*[@id=\"session_key-login\"]")
-//    private WebElement userEmailField;
-//
-//    @FindBy(xpath = "//*[@id=\"session_password-login\"]")
-//    private WebElement userPasswordField;
-//
-//    @FindBy(xpath = "//html/body/div[1]/div[1]/div/form/button")
-//    private WebElement signInButton;
+    @FindBy(xpath = "//*[@id=\"session_key-login\"]")
+    private WebElement userEmailField;
 
-    @FindBy(xpath = "//*")
-    private WebElement dummyElement;
+    @FindBy(xpath = "//*[@id=\"session_password-login\"]")
+    private WebElement userPasswordField;
+
+    @FindBy (xpath = "//*[@id=\"error-for-password\"]")
+    private  WebElement userErrorMessage;
+
+    @FindBy(xpath = "//html/body/div[1]/div[1]/div/form/button")
+    private WebElement signInButton;
+
 
     public LoginSubmit (WebDriver webDriver){
         this.webDriver = webDriver;
         PageFactory.initElements(webDriver,this);
-        //initElements();
     }
 
 //    public boolean isSignInButtonGuestHomeDisplay(){
@@ -33,15 +34,16 @@ public class LoginSubmit {
     public boolean isPageLoaded(){
         return webDriver.getCurrentUrl().contains("uas/login-submit")
                 && webDriver.getTitle().contains("Войти в LinkedIn")
-                && dummyElement.isDisplayed();
+                && signInButton.isDisplayed();
     }
 
-//    private void initElements () {
-//        userEmailField = webDriver.findElement(By.xpath("//*[@id=\"session_key-login\"]"));
-//        userPasswordField = webDriver.findElement(By.xpath("//*[@id=\"session_password-login\"]"));
-//        signInButton = webDriver.findElement(By.xpath("//*[@id=\"btn-primary\"]"));
-//
-//    }
+    public <S> S login (String userEmail, String userPassword, String emailValidationMessage, String passwordValidationMessage) {
+        userEmailField.sendKeys(userEmail);
+        userPasswordField.sendKeys(userPassword);
+        userErrorMessage.getText().contains(emailValidationMessage);
+        Assert.assertEquals(passwordValidationMessage,);
 
+
+    }
 
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -28,115 +28,68 @@ public class LoginTest {
     @DataProvider
     public Object [][] validDataProvider() {
         return new Object[][] {
-                {"SergAutoTest@bigmir.net","!qaz@wsx"},
-                {"a@b.c",""},
-                {"",""},
-                {"asada","fxfgdgd31"},
-                {"","asadsa"},
-                {"SergAutoTest@bigmir.net","112233q"}
+                {"SergAutoTest@bigmir.net","!qaz@wsx"}
         };
     }
+
+    @DataProvider
+    public Object[][] negativeValidDataProvider(){
+        return  new Object[][]{
+                {"a@b.c",""},
+                {"",""},
+                {"","asadsa"},
+
+        };
+    }
+
+    @DataProvider
+    public Object[][] loginSubmitDataProvider(){
+        return new Object[][] {
+                {"asada","fxfgdgd31","sdfs",""},
+                {"SergAutoTest@bigmir.net","112233q","",""}
+        };
+    }
+
 
     @Test(dataProvider = "validDataProvider")
     public void successfulLoginTest (String userEmail, String userPassword)  {
         webDriver.get("https://www.linkedin.com/");
-
-//        String login = "SergAutoTest@bigmir.net";
-//        String password = "!qaz@wsx";
-
         LoginPage loginPage = new LoginPage(webDriver);
-
         Assert.assertTrue(loginPage.isPageLoaded(),"Login page is not loaded!");
 
         HomePage homePage = loginPage.login(userEmail, userPassword);
-
         Assert.assertTrue(homePage.isPageLoaded(),"HomePage is not displayed on Login Page!");
 
     }
 
 
-//    @Test
-//    public void negativeLoginTestWithoutPassword () throws InterruptedException {
-//
-//        webDriver.get("https://www.linkedin.com/");
-//        String login = "a@b.c";
-//        String password = "";
-//
-//        LoginPage loginPage = new LoginPage(webDriver);
-//        Assert.assertTrue(loginPage.isPageLoaded(),"Login page is not loaded!");
-//
-//        LoginSubmit loginSubmit = loginPage.isPageLoaded(login,password);
-//        sleep(3000);
-//        Assert.assertTrue(loginSubmit.isPageLoaded(),"Your password is not empty or incorrect!");
-//
-//
-//    }
-//
-//
-//    @Test
-//    public void negativeLoginTestLoginPasswordEmpty () throws InterruptedException {
-//
-//        webDriver.get("https://www.linkedin.com/");
-//        String login = "";
-//        String password = "";
-//
-//        LoginPage loginPage = new LoginPage(webDriver);
-//        Assert.assertTrue(loginPage.isPageLoaded(),"Login page is not loaded!");
-//        loginPage.login(login,password);
-//        sleep(3000);
-//        Assert.assertTrue(loginPage.isPageLoaded(),"Your password is not empty or incorrect!");
-//
-//
-//    }
-//
-//    @Test
-//    public void negativeLoginTestLoginPasswordRandomChar () throws InterruptedException {
-//
-//        webDriver.get("https://www.linkedin.com");
-//        String login = "asada";
-//        String password = "fxfgdgd31";
-//
-//        LoginPage loginPage = new LoginPage(webDriver);
-//        Assert.assertTrue(loginPage.isPageLoaded(),"Login page is not loaded!");
-//
-//        //LoginSubmit loginSubmit = loginPage.loginSubmitPage(login,password);
-//        sleep(3000);
-//        //Assert.assertTrue(loginSubmit.isPageLoaded(),"Your password is not empty or incorrect!");
-//        //not working.
-//
-//    }
-//
-//    @Test
-//    public void negativeLoginTestLoginEmptyPasswordRandom () throws InterruptedException {
-//
-//        webDriver.get("https://www.linkedin.com/");
-//        String login = "";
-//        String password = "asadsa";
-//
-//        LoginPage loginPage = new LoginPage(webDriver);
-//        Assert.assertTrue(loginPage.isPageLoaded(),"Login page is not loaded!");
-//        loginPage.login(login,password);
-//        sleep(3000);
-//        Assert.assertTrue(loginPage.isPageLoaded(),"Your password is not empty or incorrect!");
-//
-//
-//    }
-//
-//    @Test
-//    public void negativeLoginTestWithIncorrectPassword() throws InterruptedException {
-//        webDriver.get("https://www.linkedin.com");
-//        String login = "SergAutoTest@bigmir.net";
-//        String password = "112233q";
-//
-//        LoginPage loginPage = new LoginPage(webDriver);
-//        Assert.assertTrue(loginPage.isPageLoaded(),"Login page is not loaded!");
-//        LoginSubmit loginSubmit = loginPage.loginSubmitPage(login,password);
-//
-//        //LoginSubmit loginSubmit = new LoginSubmit(webDriver);
-//        //HomePage loginSubmit = loginPage.login(login,password);
-//        sleep(3000);
-//        Assert.assertTrue(loginSubmit.isPageLoaded(),"Your password is not empty or incorrect!");
-//
-//    }
+       @Test (dataProvider = "negativeValidDataProvider")
+       public void negativeLoginTestWithoutPassword (String userEmail, String userPassword)  {
+
+           webDriver.get("https://www.linkedin.com/");
+           LoginPage loginPage = new LoginPage(webDriver);
+           Assert.assertTrue(loginPage.isPageLoaded(),"Login page is not loaded!");
+
+           loginPage.login(userEmail, userPassword);
+           Assert.assertTrue(loginPage.isPageLoaded(),"Your password is not empty or incorrect!");
+
+
+       }
+        @Test
+        public void negativeLoginTestLoginPasswordRandomChar () throws InterruptedException {
+
+            webDriver.get("https://www.linkedin.com");
+            String login = "asada";
+            String password = "fxfgdgd31";
+
+            LoginPage loginPage = new LoginPage(webDriver);
+            Assert.assertTrue(loginPage.isPageLoaded(),"Login page is not loaded!");
+
+            //LoginSubmit loginSubmit = loginPage.loginSubmitPage(login,password);
+            sleep(3000);
+            //Assert.assertTrue(loginSubmit.isPageLoaded(),"Your password is not empty or incorrect!");
+            //not working.
+
+        }
 
 }
